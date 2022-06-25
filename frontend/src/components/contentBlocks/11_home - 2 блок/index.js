afcce1eb501a88da8c2fde06e0c62e3055f1c08d
AleKoles <kolesnikova.alexandra@gmail.com>
2022-06-25 23:55:51 +0300

    added index.css and index.js

---
+++ frontend/src/components/contentBlocks/11_home - 2 блок/index.js
@@ -0,0 +1,109 @@
+import React from "react";
+import "./index.css";
+
+export default function Home2 {
+    return(
+        <div className="container">
+      <h1 className="text-capitalize text-center">How Can I help You</h1>
+      <h4 className="text-capitalize fw-normal text-center">
+        Lorem ipsum dolor sit amet mollis fellis dapibus
+      </h4>
+
+      <div className="row">
+        <div className="col-sm-6">
+          <div className="card border-0" style="max-width: 540px">
+            <div className="row g-0">
+              <div className="col-md-3">
+                <img
+                  src="./img/cloud.svg"
+                  className="img-fluid mx-auto d-block"
+                  alt="cloud"
+                />
+              </div>
+              <div className="col-md-9">
+                <div className="card-body p-0">
+                  <h5 className="card-title text-capitalize">
+                    Consultative Training
+                  </h5>
+                  <p className="card-text fw-normal text-capitalize">
+                    Reference site about Lorem Ipsum, giving information on its
+                    origins, as well as a random Lipsum genera.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6">
+          <div className="card border-0" style="max-width: 540px">
+            <div className="row g-0">
+              <div className="col-md-3">
+                <img
+                  src="./img/analytics.svg"
+                  className="img-fluid mx-auto d-block"
+                  alt="analytics"
+                />
+              </div>
+              <div className="col-md-9">
+                <div className="card-body p-0">
+                  <h5 className="card-title text-capitalize">Real deal coaching</h5>
+                  <p className="card-text fw-normal text-capitalize">
+                    Reference site about Lorem Ipsum, giving information on its
+                    origins, as well as a random Lipsum genera.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6">
+          <div className="card border-0 mb-3" style="max-width: 540px">
+            <div className="row g-0">
+              <div className="col-md-3">
+                <img
+                  src="./img/presentation.svg"
+                  className="img-fluid mx-auto d-block"
+                  alt="presentation"
+                />
+              </div>
+              <div className="col-md-9">
+                <div className="card-body p-0">
+                  <h5 className="card-title text-capitalize">
+                    Advisor training program
+                  </h5>
+                  <p className="card-text fw-normal text-capitalize">
+                    Reference site about Lorem Ipsum, giving information on its
+                    origins, as well as a random Lipsum genera.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6">
+          <div className="card border-0" style="max-width: 540px">
+            <div className="row g-0">
+              <div className="col-md-3">
+                <img
+                  src="./img/strategy.svg"
+                  className="img-fluid mx-auto d-block"
+                  alt="strategy"
+                />
+              </div>
+              <div className="col-md-9">
+                <div className="card-body p-0">
+                  <h5 className="card-title text-capitalize">Training coach</h5>
+                  <p className="card-text fw-normal text-capitalize">
+                    Reference site about Lorem Ipsum, giving information on its
+                    origins, as well as a random Lipsum genera.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    );
+}
