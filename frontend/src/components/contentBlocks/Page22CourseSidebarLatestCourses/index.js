c5e34c3de0564355b63902c51f3c7a9c81e83d3b
VlaVec <106770822+VlaVec@users.noreply.github.com>
2022-06-27 19:49:58 +0300

    Update index.js - class to className

--- frontend/src/components/contentBlocks/Page22CourseSidebarLatestCourses/index.js
+++ frontend/src/components/contentBlocks/Page22CourseSidebarLatestCourses/index.js
@@ -7,52 +7,52 @@ import svgCourseThumbnail from "./images/Course_Thumbnail.svg";
 const Page22CourseSingleV1SidebarLatestCourses = () => {
     return (
       
-      <div class="container-fluid" id="latest-courses__block">
+      <div className="container-fluid" id="latest-courses__block">
       <div class="col-12" id="latest-courses__title">Latest Courses</div>
   
-      <div class="row" id="latest-courses__list">
-          <div class="col-4" id="latest-courses__image-block"><a href="#"><img src="{ svgCourseThumbnail }" class="img-fluid" alt="course-icon" id="latest-courses__image" /></a></div>
-          <div class="col-7 align-middle" id="latest-courses__texts">
-              <div class="col-12" id="course-name"><a href="#" class="course-name__link">Web Development and Design</a></div>
-              <div class="row" id="latest_courses_prices">
-                  <div class="col-2" id="latest-courses__old-price">$959</div>
-                  <div class="col-10" id="latest-courses__new-price">$415.99</div>
+      <div className="row" id="latest-courses__list">
+          <div className="col-4" id="latest-courses__image-block"><a href="#"><img src="{ svgCourseThumbnail }" className="img-fluid" alt="course-icon" id="latest-courses__image" /></a></div>
+          <div className="col-7 align-middle" id="latest-courses__texts">
+              <div className="col-12" id="course-name"><a href="#" className="course-name__link">Web Development and Design</a></div>
+              <div className="row" id="latest_courses_prices">
+                  <div className="col-2" id="latest-courses__old-price">$959</div>
+                  <div className="col-10" id="latest-courses__new-price">$415.99</div>
               </div>
   
            </div>
       </div>
   
-      <div class="row" id="latest-courses__list">
-          <div class="col-4" id="latest-courses__image-block"><a href="#"><img src="{ svgCourseThumbnail }" class="img-fluid" alt="course-icon" id="latest-courses__image" /></a></div>
-          <div class="col-7" id="latest-courses__texts">
-              <div class="col-12" id="course-name"><a href="#" class="course-name__link">The Complete Cyber Security Course : Hackers</a></div>
-              <div class="row" id="latest_courses_prices">
-                  <div class="col-2" id="latest-courses__old-price">$959</div>
-                  <div class="col-10" id="latest-courses__new-price">$415.99</div>
+      <div className="row" id="latest-courses__list">
+          <div className="col-4" id="latest-courses__image-block"><a href="#"><img src="{ svgCourseThumbnail }" className="img-fluid" alt="course-icon" id="latest-courses__image" /></a></div>
+          <div className="col-7" id="latest-courses__texts">
+              <div className="col-12" id="course-name"><a href="#" className="course-name__link">The Complete Cyber Security Course : Hackers</a></div>
+              <div className="row" id="latest_courses_prices">
+                  <div className="col-2" id="latest-courses__old-price">$959</div>
+                  <div className="col-10" id="latest-courses__new-price">$415.99</div>
               </div>
   
            </div>
       </div>
   
-      <div class="row" id="latest-courses__list">
-          <div class="col-4" id="latest-courses__image-block"><a href="#"><img src="{ svgCourseThumbnail }" class="img-fluid" alt="course-icon" id="latest-courses__image" /></a></div>
-          <div class="col-7" id="latest-courses__texts">
-              <div class="col-12" id="course-name"><a href="#" class="course-name__link">Fashion Photography From Professional</a></div>
-              <div class="row" id="latest_courses_prices">
-                  <div class="col-2" id="latest-courses__old-price">$959</div>
-                  <div class="col-10" id="latest-courses__new-price">$415.99</div>
+      <div className="row" id="latest-courses__list">
+          <div className="col-4" id="latest-courses__image-block"><a href="#"><img src="{ svgCourseThumbnail }" className="img-fluid" alt="course-icon" id="latest-courses__image" /></a></div>
+          <div className="col-7" id="latest-courses__texts">
+              <div className="col-12" id="course-name"><a href="#" className="course-name__link">Fashion Photography From Professional</a></div>
+              <div className="row" id="latest_courses_prices">
+                  <div className="col-2" id="latest-courses__old-price">$959</div>
+                  <div className="col-10" id="latest-courses__new-price">$415.99</div>
               </div>
   
            </div>
       </div>
   
-      <div class="row" id="latest-courses__list">
-          <div class="col-4" id="latest-courses__image-block"><a href="#"><img src="{ svgCourseThumbnail }" class="img-fluid" alt="course_icon" id="latest-courses__image" /></a></div>
-          <div class="col-7" id="latest-courses__texts">
-              <div class="col-12" id="course-name"><a href="#" class="course-name__link">The Complete Financial Analyst Course 2020</a></div>
-              <div class="row" id="latest_courses_prices">
-                  <div class="col-2" id="latest-courses__old-price">$959</div>
-                  <div class="col-10" id="latest-courses__new-price">$415.99</div>
+      <div className="row" id="latest-courses__list">
+          <div className="col-4" id="latest-courses__image-block"><a href="#"><img src="{ svgCourseThumbnail }" className="img-fluid" alt="course_icon" id="latest-courses__image" /></a></div>
+          <div className="col-7" id="latest-courses__texts">
+              <div className="col-12" id="course-name"><a href="#" className="course-name__link">The Complete Financial Analyst Course 2020</a></div>
+              <div className="row" id="latest_courses_prices">
+                  <div className="col-2" id="latest-courses__old-price">$959</div>
+                  <div className="col-10" id="latest-courses__new-price">$415.99</div>
               </div>
   
            </div>
