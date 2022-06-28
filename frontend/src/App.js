c1506467ba6ee5b6ff8ffc1629781d54fc8f24e2
Mari <mari.ashikhmina@gmail.com>
2022-06-28 18:39:33 +0300

    change class to className again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import Just11home5block from './components/contentBlocks/Just11home5block';
 
 function App() {
   return (
     <>
-    <Just11home5block />
   
     </>
   );
