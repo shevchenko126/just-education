7188af4b85936e2a5dc213f5ed4b909ffee1f00d
oleh-l <106692215+oleh-l@users.noreply.github.com>
2022-07-01 13:40:24 +0300

    Courses + Course Chapter, Course Lecture

--- backend/courses/models.py
+++ backend/courses/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import timedelta
 
 
 class Course(models.Model):
@@ -21,3 +22,47 @@ class Course(models.Model):
     class Meta:
         verbose_name = "Курс"
         verbose_name_plural = "Курси"
+
+
+class CourseChapter(models.Model):
+    chapter_title = models.CharField(verbose_name="Назва Розділу",
+                                     max_length=100,)
+    duration = models.DurationField(verbose_name="Тривалість",
+                                    default=timedelta(days=1),
+                                    help_text=('(дні год:хв:сек)'),
+                                    blank=True,)
+    course = models.ForeignKey(Course, verbose_name="Курс",
+                               related_name="course_chapters",
+                               on_delete=models.CASCADE,
+                               blank=True,
+                               null=True)
+
+    def __str__(self):
+        return self.chapter_title
+
+    class Meta:
+        verbose_name = "Розділ курсу"
+        verbose_name_plural = "Розділи курсів"
+
+
+class CourseLecture(models.Model):
+    lecture_title = models.CharField(verbose_name="Назва Лекції",
+                                     max_length=100,)
+    duration = models.DurationField(verbose_name="Тривалість",
+                                    default=timedelta(minutes=25),
+                                    help_text=('(дні год:хв:сек)'),
+                                    blank=True,
+                                    null=True)
+    course_chapter = models.ForeignKey(CourseChapter,
+                                       verbose_name="Розділ курсу",
+                                       related_name="course_lectures",
+                                       on_delete=models.CASCADE,
+                                       blank=True,
+                                       null=True)
+
+    def __str__(self):
+        return self.lecture_title
+
+    class Meta:
+        verbose_name = "Лекція курсу"
+        verbose_name_plural = "Лекції курсів"
