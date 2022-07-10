434227de5a0458ea1d300694315e0c6d1292bf3d
Maksym German <maxjerman@gmail.com>
2022-07-10 19:41:21 +0300

    fixed colour

--- frontend/src/components/contentBlocks/68_register/index.js
+++ frontend/src/components/contentBlocks/68_register/index.js
@@ -8,7 +8,7 @@ const register = () => {
     return (
         <div className="register_block container">
             <div className="row justify-content-center">
-                <div className='col-4'>
+                <div className='col-6'>
                     <div className='headers_section'>
                         <h1>Register</h1>
                         <p>Home   /   Register </p>
