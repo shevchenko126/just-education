f3693d6c78157123663bb0b816864b5b8658b830
MichaelFadyeyev <mihail.fadeev@ukr.net>
2022-07-11 11:03:35 +0300

    qwestion top finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,17 +7,19 @@ import Instructor from './components/CoursesSingle/Instructor';
 import Just11home5block from './components/contentBlocks/Just11home5block';
 import '../src/components/contentBlocks/commonCss/styles.css';
 import FeaturedCourses from './components/contentBlocks/11home3block';
+import Quiz from './components/contentBlocks/42_quiz'
 
 
 function App() {
   return (
     <>
-      <Header />
-      <FeaturedCourses/>
+      <Quiz/>
+      {/* <FeaturedCourses/> */}
+      {/* <Header />
       <InstructorsSingle />
       <Tabs />
       <Instructor />
-      <Just11home5block />
+      <Just11home5block /> */}
     </>
   );
 }
--- frontend/src/components/contentBlocks/42_quiz/index.js
+++ frontend/src/components/contentBlocks/42_quiz/index.js
@@ -3,11 +3,31 @@ import './styles.css';
 
 const Quiz = () => {
     return(
-<div className="container">
-    <div className="row">
-        <div className="col">222</div>
-    </div>
-</div>
+        <div className="container__quiz">
+            <div className="quiz__title h-1">Quiz</div>
+            <div className="quiz__subtitle p">Ready to go?</div>
+            <div className="container__questions">
+                <div className="question__item">
+                    <div className="question__item-question">
+                        <div className="question__attributes">
+                            <div className="question__attributes-title h-3">Question #</div>
+                            <div className="question__attributes-interact p">
+                                <div>Flag Question</div>
+                                <div>Edit Question</div>
+                            </div>
+                        </div>
+                        <div className="question_properties p">
+                            <div>Not yet answered</div>
+                            <div>Marked out of 1.00</div>
+                        </div>
+                        <div className="question__subject h-2">You are watching the TV news and see this appear on the screen:</div>
+                    </div>
+                    <div className="question__item-unswer">
+
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
