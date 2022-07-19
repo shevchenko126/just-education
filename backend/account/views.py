6a7c36f1c6829cff792329c18aa7784d6b12c1c6
oleh-l <106692215+oleh-l@users.noreply.github.com>
2022-07-19 21:40:19 +0300

    Authorization in account app. Получаем данные, авторизуем пользователя, выдаем токен

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
 
-# Create your views here.
+
+class CustomAuthData(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+
+        user_data = {
+            "username": request.data['username'].lower(),
+            "password": request.data['password']
+        }
+
+        try:
+            user = authenticate(**user_data)
+            resp = {
+                    "sucess": True,
+                    "token": Token.objects.filter(user=user).first().key,
+                    "user_id": user.id,
+                    "user_email": user.email,
+                    "is_teacher": user.profile.is_teacher,
+                    "is_student": user.profile.is_student
+                    },
+        except:
+            resp = {
+                "sucess": False,
+                "toke": "",
+                "user_id": 0,
+                "user_email": "",
+                "is_teacher": False,
+                "is_student": False
+            }
+        return Response(resp)
+
+    def get(self, request, *args, **kwargs):
+
+        token = request.data['token']
+        try:
+            user = Token.objects.get(key=token).user
+            user_id = user.id
+            user_email = user.email
+        except Token.DoesNotExist:
+            user_id = 0
+            user_email = ''
+
+        return Response({
+            "token": token,
+            "user_id": user_id,
+            "user_email": user_email,
+            "is_teacher": user.profile.is_teacher,
+            "is_student": user.profile.is_student
+        })
--- backend/just/settings.py
+++ backend/just/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'courses',
     'account',
     'rest_framework',
+    'rest_framework.authtoken',
     'registration',
 
     'notifications',
@@ -130,3 +131,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
