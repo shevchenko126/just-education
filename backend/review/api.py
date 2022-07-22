393fcf61abacbcc39d2a48596a2647fee7e37499
Maksym German <maxjerman@gmail.com>
2022-07-22 21:42:03 +0300

    Review API: create review after auth. and various validations

--- backend/review/api.py
+++ backend/review/api.py
@@ -1,5 +1,7 @@
 from rest_framework import viewsets, exceptions, pagination
 from rest_framework.response import Response
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import IntegrityError
 
 from courses.models import Course
 from review.models import Review
@@ -16,6 +18,7 @@ class GetReviews(viewsets.ModelViewSet):
     pagination_class = ReviewPaginator
 
     def list(self, request, *args, **kwargs):
+        """Return paginated reviews filtered by query-params 'course_id' and 'page'"""
         page = self.paginate_queryset(self.get_queryset())
         serializer = self.get_serializer(page, many=True)
         return Response({'success': True,
@@ -33,3 +36,20 @@ class GetReviews(viewsets.ModelViewSet):
             else:
                 raise exceptions.NotFound('No such course')
         return self.queryset.all()
+
+    def create(self, request, *args, **kwargs):
+        """ Create review by the current user if he is registered to the course. Maximum one per user per course"""
+        if request.auth is None:
+            raise exceptions.NotAuthenticated
+
+        course_id = request.data.get('course_id')
+        try:
+            request.user.student.registrations.get(course_id=course_id)
+        except (AttributeError, ObjectDoesNotExist):
+            raise exceptions.PermissionDenied('You are not a student of this course')
+        except ValueError:
+            raise exceptions.ValidationError('The course_id must be an integer')
+        try:
+            return super().create(request, *args, **kwargs)
+        except IntegrityError:
+            raise exceptions.PermissionDenied('You have already rated this course')
--- backend/review/serializers.py
+++ backend/review/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
+
+from courses.models import Course
 from .models import Review
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    course_id = serializers.PrimaryKeyRelatedField(source='course', queryset=Course.objects.all())
+    user_id = serializers.PrimaryKeyRelatedField(read_only=True, default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Review
-        fields = ["course", 'title', 'description', 'rating', 'date']
+        fields = ['course_id', 'user_id', 'title', 'description', 'rating', 'date']
+
+    def save(self, **kwargs):
+        kwargs['user'] = self.fields['user_id'].get_default()
+        super().save(**kwargs)
