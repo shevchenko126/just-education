a529a4a809e406fee2960ef1f8cc9f325afd5d93
Maksym German <maxjerman@gmail.com>
2022-07-22 21:35:01 +0300

    Registration model: added related_name for course, student fields

--- backend/registration/models.py
+++ backend/registration/models.py
@@ -1,9 +1,5 @@
 from datetime import datetime
-from xmlrpc.client import DateTime
 from django.db import models
-from email import message
-from django.conf import Settings
-from django.contrib.auth.models import User
 
 from courses.models import Course
 from account.models import Student
@@ -12,18 +8,19 @@ from account.models import Student
 class CourseRegistration(models.Model):
     course = models.ForeignKey(Course, verbose_name="Курс",
                                on_delete=models.SET_NULL,
+                               related_name='registrations',
                                blank=True,
                                null=True)
     student = models.ForeignKey(Student, verbose_name="Студент",
-                               on_delete=models.SET_NULL,
-                               blank=True,
-                               null=True)
+                                on_delete=models.SET_NULL,
+                                related_name='registrations',
+                                blank=True,
+                                null=True)
     data = datetime.now
-    
-      
+
     def __str__(self):
         return f'Registration({self.course}, {self.student}, {self.data})'
-    
+
     class Meta:
-        verbose_name ="Регистрація"
-        verbose_name_plural ="Регистрації"   
+        verbose_name = "Регистрація"
+        verbose_name_plural = "Регистрації"
