68a55e9951e280ba0f6b506934c0f176e8619c35
Maksym German <maxjerman@gmail.com>
2022-07-24 00:43:17 +0300

    Registration model fix + custom admin

--- backend/registration/models.py
+++ backend/registration/models.py
@@ -1,26 +1,25 @@
-from datetime import datetime
 from django.db import models
+from django.db.models import UniqueConstraint
 
 from courses.models import Course
 from account.models import Student
 
-# Create your models here.
+
 class CourseRegistration(models.Model):
-    course = models.ForeignKey(Course, verbose_name="Курс",
-                               on_delete=models.SET_NULL,
-                               related_name='registrations',
-                               blank=True,
-                               null=True)
-    student = models.ForeignKey(Student, verbose_name="Студент",
-                                on_delete=models.SET_NULL,
-                                related_name='registrations',
-                                blank=True,
-                                null=True)
-    data = datetime.now
+    course = models.ForeignKey(Course, verbose_name='курс',
+                               on_delete=models.CASCADE,
+                               related_name='registrations')
+    student = models.ForeignKey(Student, verbose_name='студент',
+                                on_delete=models.CASCADE,
+                                related_name='registrations')
+    date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'Registration({self.course}, {self.student}, {self.data})'
+        return f'реєстрація ({self.course}, {self.student})'
 
     class Meta:
-        verbose_name = "Регистрація"
-        verbose_name_plural = "Регистрації"
+        verbose_name = 'реєстрація'
+        verbose_name_plural = 'реєстрації'
+        constraints = [
+            UniqueConstraint(fields=('course', 'student'), name='one_reg_per_student')
+        ]
